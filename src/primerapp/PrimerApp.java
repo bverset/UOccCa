4fd9dc89dd12f5892753ae476fd66f62f2b33a54
berna <bernard.verset@appsare.us>
2016-11-12 01:37:54 +0100

    primer commit

---
+++ src/primerapp/PrimerApp.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerapp;
+
+/**
+ *
+ * @author berna
+ */
+public class PrimerApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double primerparcial;
+        primerparcial = 15*4.2;
+        double segundoparcial;
+        segundoparcial = 15*3.8;
+        double tercerparcial;
+        tercerparcial = 20*4;
+        double quartoparcial;
+        quartoparcial = 20*3.2;
+        double quintoparcial;
+        quintoparcial = 20*3.9;
+        double sextoparcial;
+        sextoparcial = 10*4.8;
+        
+        double calificacionFinal;
+        calificacionFinal = (primerparcial+segundoparcial+tercerparcial+quartoparcial+quintoparcial+sextoparcial)/100;
+        
+        System.out.println("Cuotacion final:" + calificacionFinal);
+    }
+    
+}
