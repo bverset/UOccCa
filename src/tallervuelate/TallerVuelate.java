4b19d7053dd2a1a42ac0153c7a636657a875b5cf
berna <bernard.verset@appsare.us>
2016-12-10 02:56:59 +0100

    commit

---
+++ src/tallervuelate/TallerVuelate.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallervuelate;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author berna
+ */
+public class TallerVuelate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MenuGui menugui = new MenuGui(new javax.swing.JFrame(), true);
+        menugui.setVisible(true);
+    }
+    
+}
